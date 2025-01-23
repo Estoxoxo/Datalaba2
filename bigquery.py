e9f6b6558b572732306fc2c659aea3ea31fcb7b6
Estoxoxo <155571524+Estoxoxo@users.noreply.github.com>
2025-01-23 16:13:01 -0600

    Update bigquery.py

--- bigquery.py
+++ bigquery.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 # Ajustar la ruta a las credenciales
 
-credentials_path = ''
+credentials_path = 'Bigquery'
 
 credentials = service_account.Credentials.from_service_account_file(credentials_path)
 
